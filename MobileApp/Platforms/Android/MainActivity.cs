5a1c76354aac03fbf656d84943b8b83d7b8aa042
Radu Ovidiu <ovidiuradu93@gmail.com>
2026-04-26 15:18:59 +0300

    Fix Android launcher icon by adding MainActivity

---
+++ MobileApp/Platforms/Android/MainActivity.cs
@@ -0,0 +1,10 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace ProiectVentilatie.Mobile;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, Exported = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
